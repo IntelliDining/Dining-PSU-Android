9f97a4c8cc16e99f6a0c0211e853c42136e579e9
William Gulian <williamgulian@gmail.com>
2018-10-07 00:08:59 -0400

    Add detail page for dining hall

--- app/src/main/java/intellidining/diningpsu/models.kt
+++ app/src/main/java/intellidining/diningpsu/models.kt
@@ -7,3 +7,6 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class DiningHall(val name: String, val imageIcon: Uri? = null) : Parcelable
 
+@Parcelize
+data class MenuItem(val name: String) : Parcelable
+
